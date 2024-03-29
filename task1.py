340fdc1474999953da9d65ad1c1468a6f74027ef
Mihhail Tun <ktunec5@gmail.com>
2024-03-29 11:53:48 +0000

    Classes_heredity_наследственнось

--- task1.py
+++ task1.py
@@ -18,7 +18,30 @@ class Employee:
     def __str__(self):
         return 'Я объект employee, принадлежащий классу Employee.'
 
+    def sound(self):
+        print('Работаю!')
+
 
 employee = Employee('Роберт', 'Крузо', 'м')
+employee_2 = Employee('Vira', 'Tun', 'f')
+employee_2.consume_vacation(1)
 
-print(employee)
+
+class TeamLead(Employee):
+    team = 40
+    vacation_days = 300
+
+    def __init__(self, first_name, second_name, gender, weight):
+        super().__init__(first_name, second_name, gender)
+        self.weight = weight
+
+    def sound(self):
+        print('Работать!')
+
+
+teamlead = TeamLead('Mike', 'Tun', 'm', 92)
+mlead = TeamLead('Vira', 'Tun', 'f', 80)
+
+teamlead.first_name = 'Mihhail'
+print(mlead.weight, mlead.first_name, mlead.second_name)
+
